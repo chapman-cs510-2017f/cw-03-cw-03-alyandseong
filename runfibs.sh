7e491a8e303a90ead791a8430876cc392da34617
alycebaughman <baugh107@mail.chapman.edu>
2017-09-19 10:15:07 -0700

    third draft

--- runfibs.sh
+++ runfibs.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-
+ 
 #fib.py for arguments 1 to 10000
-
-if [-f fibs.csv.bak]
+ 
+if [ -f fibs.csv.bak]
 then
     echo "Error- file and back-up file already exist"
     exit 1
-
-elif [-f fibs.csv]
+fi
+ 
+if [ -f fibs.csv]
 then
     mv fibs.csv fibs.csv.bak
-
+ 
 else
     for n in {1..10000}; do
-
-    ./fib.py $n >> fibs.csv
-
+    echo -n "$(./fib.py $n),"
+ 
     done
-
-# for the csv file:
-
-# remove new lines in csv file and switch to commas
-# echo a delete character
-echo -n ''#special code to delete ,'' # creates comma
-
+fi
