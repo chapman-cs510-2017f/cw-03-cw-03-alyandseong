668a36c27387a2ba7c2add5173318d2ec54a02be
Aly Baughman <baugh107@mail.chapman.edu>
2017-09-19 18:09:06 +0000

    updated draft

--- runfibs.sh
+++ runfibs.sh
@@ -2,19 +2,18 @@
  
 #fib.py for arguments 1 to 10000
  
-if [ -f fibs.csv.bak]
+if [ -f "fibs.csv.bak" ]
 then
     echo "Error- file and back-up file already exist"
     exit 1
 fi
  
-if [ -f fibs.csv]
+if [ -f "fibs.csv" ]
 then
     mv fibs.csv fibs.csv.bak
- 
+    echo "Back-up file created: file fibs.csv.back"
 else
     for n in {1..10000}; do
-    echo -n "$(./fib.py $n),"
- 
+    echo -n "$(./fib.py $n)," >> fibs.csv
     done
 fi
